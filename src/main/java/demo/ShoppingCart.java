b7d5522d94c3eb6f34c0e0d1a0660b670ef8323d
Kishan-Kumar-Zalavadia <kishankumarz1048@gmail.com>
2023-01-12 15:04:26 +0530

    Before and After Point cuts in an Aspect

--- src/main/java/demo/ShoppingCart.java
+++ src/main/java/demo/ShoppingCart.java
@@ -4,7 +4,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ShoppingCart {
-    public void checkout(){
+    public void checkout(String status){
+        //Logging
+        //Authentication and Authorization
+        //Sanitize the data
+
+        //All the above are Cross-Cutting-Concerns (Each one is known as an aspect)
+
+        //The below is the Business Login
+
         System.out.println("Checkout method from shopping cart called");
+
+        //AOP is used to separate Business-Login with Cross-Cutting-Concerns
+
     }
 }
